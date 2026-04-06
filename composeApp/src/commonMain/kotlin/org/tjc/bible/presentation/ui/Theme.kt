bb2aebf32bd8e487504117fd9f78135e2d089cee
Larry Wang <larrywang10@gmail.com>
2026-04-05 23:55:34 -0700

    feat(ui): use MaterialExpressiveTheme

--- composeApp/src/commonMain/kotlin/org/tjc/bible/presentation/ui/Theme.kt
+++ composeApp/src/commonMain/kotlin/org/tjc/bible/presentation/ui/Theme.kt
@@ -2,12 +2,14 @@ package org.tjc.bible.presentation.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 expect fun platformColorScheme(darkTheme: Boolean, dynamicColor: Boolean): ColorScheme
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun BibleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -16,7 +18,7 @@ fun BibleTheme(
 ) {
     val colorScheme = platformColorScheme(darkTheme, dynamicColor)
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         content = content
     )
