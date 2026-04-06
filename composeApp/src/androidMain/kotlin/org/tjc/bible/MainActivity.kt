06b9a23a4516b19e7d0f8566007f8f238b708c69
Larry Wang <larrywang10@gmail.com>
2026-04-05 23:37:25 -0700

    fix(ui): show the icons in the status bar

--- composeApp/src/androidMain/kotlin/org/tjc/bible/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/tjc/bible/MainActivity.kt
@@ -1,11 +1,21 @@
 package org.tjc.bible
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import org.koin.compose.viewmodel.koinViewModel
+import org.tjc.bible.domain.model.AppTheme
+import org.tjc.bible.presentation.bible.BibleViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +23,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val viewModel: BibleViewModel = koinViewModel()
+            val state by viewModel.state.collectAsState()
+            val darkTheme = when (state.theme) {
+                AppTheme.SYSTEM -> isSystemInDarkTheme()
+                AppTheme.LIGHT -> false
+                AppTheme.DARK -> true
+            }
+
+            val view = LocalView.current
+            if (!view.isInEditMode) {
+                SideEffect {
+                    val window = (view.context as Activity).window
+                    WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+                }
+            }
+
             App()
         }
     }
